c0b25669d15228bc146a98b529e66f69352b8ddf
jake howlett <jake.howlett93@gmail.com>
2021-02-15 11:48:33 +0000

    added logic to the answer action, and made the view look a bit better

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,16 @@ class QuestionsController < ApplicationController
   def ask 
   end
 
-  def answer  
+  def answer
+    if params[:question]
+      @question = params[:question]
+    end
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
